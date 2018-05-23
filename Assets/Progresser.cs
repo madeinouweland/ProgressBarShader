7271dc3da702dae9d055d21ef463be691ee2f308
hauptstadtcoder <hauptstadtcoder@fastmail.com>
2018-05-23 21:29:40 +0200

    make it so:

---
+++ Assets/Progresser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Progresser : MonoBehaviour
+{
+
+	private Material _material;
+	private float _value;
+
+	private void Start()
+	{
+		_material = GetComponent<MeshRenderer>().material;
+	}
+
+	private void Update()
+	{
+		_value += Time.deltaTime;
+		if (_value > 1)
+		{
+			_value = 0;
+		}
+		_material.SetFloat("_Value", _value);
+	}
+
+}
